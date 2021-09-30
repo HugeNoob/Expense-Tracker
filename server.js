93e258eb91370e949e346ab071623a743910db84
HugeNoob <thelastoreointhejar@gmail.com>
2021-09-30 16:13:03 +0800

    added express redirect

--- server.js
+++ server.js
@@ -27,3 +27,7 @@ if(process.env.NODE_ENV === 'production'){
 // SERVER LISTENING ON HOST OR PORT 5000
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` ));
+
+app.all('*', function(req, res) {
+    res.redirect("/");
+  });
